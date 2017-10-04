dc07d5477876793afaa27c9b84719b4b230324cc
EliGarcia <garciaelisha93@yahoo.com>
2017-10-04 17:34:42 -0500

    done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,46 +3,53 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import static com.sun.tools.doclint.Entity.lambda;
 import static jdk.nashorn.internal.objects.Global.print;
 
-
-public class ComparatorDemo {
+public class Main {
 
     public static void main(String[] args) {
+
+        Runnable anonymousRun = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("my anonymous class");
+            }
+        };
+        anonymousRun.run();
+
+
+        Runnable lambdaRun = (() -> {
+            System.out.println("anonymous lambda function");
+        });
+        lambdaRun.run();
+
         List<Address> addresses = new ArrayList<>();
         addresses.add(new Address("123 Main St.", "Austin", "TX", "78787"));
         addresses.add(new Address("43 High St.", "Columbus", "OH", "43034"));
         addresses.add(new Address("678 Leon Rd.", "Tallahassee", "FL", "32307"));
         addresses.add(new Address("8 Deep Sea Way", "Boca Raton", "FL", "33433"));
 
-
         System.out.println("Before sorting");
-        print(addresses);
+        System.out.println(addresses);
 
         Collections.sort(addresses, new Comparator<Address>() {
             @Override
             public int compare(Address address1, Address address2) {
                 String address1State = address1.getState();
-                String address22State = address2.getState();
-                int compare = address1State.compareTo(address22State;
+                String address2State = address2.getState();
+                int compare = address1State.compareTo(address2State);
                 return compare;
             }
         });
 
         System.out.println("\nAfter sorting by state");
-        print(addresses);
+        System.out.println(addresses);
 
-        // !! - Write a lambda to sort by city alphabetically
-        Collections.sort(addresses, /* lambda here */);
+        Collections.sort(addresses, (Address address1, Address address2) ->
+                address1.getCity().compareTo(address2.getCity())
+        );
 
         System.out.println("\nAfter sorting by city");
-        print(addresses);
-    }
-
-    private static void print(List<Address> addresses) {
-        for(Address address : addresses) {
-            System.out.println(address);
-        }
+        System.out.println(addresses);
     }
 }
